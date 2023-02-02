f78601ef7d1c57d7c2f8e0342dcce99c1207a1a6
JohnMigwi <johnmigwi101@gmail.com>
2023-02-02 21:17:57 +0300

    "fetch error but atleast its a diffrent one"

--- pages/app.js
+++ pages/app.js
@@ -1,13 +1,13 @@
 import React,{ useEffect, useState } from 'react';
 import { layout } from '../components';
 import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
+
 import Layout from '../components/layout';
 
 
 
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps } ) {
   return(
     <Layout>
       <Component {...pageProps} />
